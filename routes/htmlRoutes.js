77429b5eddda3a126af1032dcadd1dcd643e2b86
JD <jdfili14@gmail.com>
2019-02-25 19:55:13 -0800

    database api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,27 +1,25 @@
+// eslint-disable-next-line no-unused-vars
 var db = require("../models");
-
+var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
-    });
-  });
-
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
-  });
-
-  // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
-    res.render("404");
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
+// Load example page and pass in an example by id
+//   app.get("/example/:id", function(req, res) {
+//     db.Example.findOne({ where: { id: req.params.id } }).then(function(
+//       dbExample
+//     ) {
+//       res.render("example", {
+//         example: dbExample
+//       });
+//     });
+//   });
+
+//   // Render 404 page for any unmatched routes
+//   app.get("*", function(req, res) {
+//     res.render("404");
+//   });
+// };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
+// var exphbs = require("express-handlebars");
 
 var db = require("./models");
 
@@ -13,13 +13,13 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
-app.set("view engine", "handlebars");
+// app.engine(
+//   "handlebars",
+//   exphbs({
+//     defaultLayout: "main"
+//   })
+// );
+// app.set("view engine", "handlebars");
 
 // Routes
 require("./routes/apiRoutes")(app);
