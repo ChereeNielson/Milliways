051434724a30f18245941d47203d22d26a943aaf
JD <jdfili14@gmail.com>
2019-02-25 20:38:00 -0800

    readded handlebars :(

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,18 +6,17 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
-};
-// Load example page and pass in an example by id
-//   app.get("/example/:id", function(req, res) {
-//     db.Example.findOne({ where: { id: req.params.id } }).then(function(
-//       dbExample
-//     ) {
-//       res.render("example", {
-//         example: dbExample
-//       });
-//     });
-//   });
 
+  // Load example page and pass in an example by id
+  app.get("/reservations", function(req, res) {
+    db.Reservation.findAll({}).then(function(dbReservation) {
+      var hbsobject = {
+        reservations: dbReservation
+      };
+      res.render("index", hbsobject);
+    });
+  });
+};
 //   // Render 404 page for any unmatched routes
 //   app.get("*", function(req, res) {
 //     res.render("404");
